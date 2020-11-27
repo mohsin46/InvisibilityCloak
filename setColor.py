bc82256231855f64c22057065cc293841fcf7163
Mohsin Shaikh <66210928+mohsin46@users.noreply.github.com>
2020-11-27 17:29:51 +0530

    Update setColor.py

--- setColor.py
+++ setColor.py
@@ -8,9 +8,9 @@ def nothing(x):
 
 cap = cv2.VideoCapture(0)
 
-cv2.namedWindow("test")
+cv2.namedWindow("test")         # creates empty window for trackbars
 
-cv2.createTrackbar("l_h", "test", 98, 255, nothing)
+cv2.createTrackbar("l_h", "test", 98, 255, nothing)   # creates trackbars
 cv2.createTrackbar("l_s", "test", 185, 255, nothing)
 cv2.createTrackbar("l_v", "test", 67, 255, nothing)
 
@@ -20,9 +20,9 @@ cv2.createTrackbar("u_v", "test", 206, 255, nothing)
 
 while True:
     ret, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   # converts BGR feed to HSV
 
-    lH = cv2.getTrackbarPos("l_h", "test")
+    lH = cv2.getTrackbarPos("l_h", "test")     # gets trackbar position
     lS = cv2.getTrackbarPos("l_s", "test")
     lV = cv2.getTrackbarPos("l_v", "test")
 
@@ -30,12 +30,12 @@ while True:
     uS = cv2.getTrackbarPos("u_s", "test")
     uV = cv2.getTrackbarPos("u_v", "test")
 
-    lowerBlue = np.array([lH, lS, lV])
+    lowerBlue = np.array([lH, lS, lV])   # sets the hsv value derived from trackbar
     upperBlue = np.array([uH, uS, uV])
 
-    mask = cv2.inRange(hsv, lowerBlue, upperBlue)
+    mask = cv2.inRange(hsv, lowerBlue, upperBlue)  # creates mask for cloak
 
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+    res = cv2.bitwise_and(frame, frame, mask=mask) 
 
     cv2.imshow("final", res)
     cv2.imshow("frame",frame)
